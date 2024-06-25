0a1452ca7e6b10f53cc4aeed51347178bba0559c
Niels Burghoorn <burghoornniels@gmail.com>
2024-06-25 08:18:23 +0200

    "test server for render"

---
+++ SAFE/test_server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_from_directory, jsonify, request
+import flask_cors
+import math
+import numpy as np
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='./assets',
+            template_folder='./templates')
+flask_cors.CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('chart.html')
